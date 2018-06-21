4a3fcffaf7cbec45a952781438e1305d568ca456
李振东 <lzdong@foxmail.com>
2018-06-21 22:23:36 +0800

    雷振东的修改—2

---
+++ Application/Home/Conf/config.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+	//'配置项'=>'配置值'
+
+	//微信开发配置文件
+	// 'TOKEN' 		=> 'penalty',
+ //  	'APPID'     	=> 'wxae48f3bbcda86ab1',
+ //  	'APPSECRET' 	=> '1c39100b331713ae9e96a4e5eb470424',
+ //  	'MCHID'     	=> '1480327842',
+ //  	'KEY'       	=> 'CAA5EAE2CE5AC44A3F8930E6F127B423',
+ //  	'notify_url'	=> 'http://pub.dianqiukj.com/index.php/Home/PaymentSystem/notify.html',
+
+
+    'TOKEN'     => 'penalty',
+    'APPID'       => 'wx27fe92c4c77f7df6',
+    'APPSECRET'   => 'bfe715cace634d85e6b5e17a93843de6',
+    'MCHID'       => '1310058501',
+    'KEY'         => '36d04a9d74392c727b1a9bf97a7bcbac',
+    'notify_url'  => 'http://hanpai2.dianqiukj.com/index.php/Home/PaymentSystem/notify.html',
+  	// 开启layout模板布局
+  	'LAYOUT_ON'=>true,
+	'LAYOUT_NAME'=>'Layout/layout1',
+);
