0660ec4acd481f560e72816bba5160951a1c5d8c
李振东 <lzdong@foxmail.com>
2018-06-22 22:26:24 +0800

    微信

---
+++ Application/Common/Tool/WeiXin.class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 李振东
+ * Time: 2018/3/29 下午2:37
+ */
+namespace Common\Tool;
+use \Org\Util\WeixinJssdk;
+
+use Think\Log;
+class WeiXin
+{
+    public $APPID;
+    public $APPSECRET;
+    public $MCHID;
+    public $KEY;
+
+    // 微信对象
+    public function __construct($option)
+    {
+
+    }
+
+}
